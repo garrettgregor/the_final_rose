b6e655c53e537464503d815210c6e25df7a17194
Garrett Gregor <garrett.gregor@gmail.com>
2023-06-11 10:44:32 -0600

    US1: Feat: bachelorette contestants index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :bachelorettes, only: [:show] do
-    resources :contestants, only: [:index]
+    resources :contestants, only: [:index], controller: "bachelorette_contestants"
   end
 end
